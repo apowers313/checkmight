1b013b7ab525614005045c88180fb81edbbd4668
Adam Powers <apowers@ato.ms>
2020-10-25 09:45:18 -0700

    update documentation support

--- bin/mkdocs.js
+++ bin/mkdocs.js
@@ -1,15 +1,17 @@
-const {plugin} = require("..");
 const {inspect} = require("util");
+const fs = require("fs");
+const TEMPLATE_FILE = "template/README-template.md";
+const URL_ROOT = "https://apowers313.github.io/checkmight/";
+// const URL_ROOT = "";
+const OUTPUT_FOLDER = "docs";
 
-// TODO: parse command line arguments, get output file name
-let print = console.log;
-
-let textEntry = new Map();
+let print = function(fd, str) {
+    fs.writeSync(fd, `${str}\n`, "utf8");
+};
+let warn = console.error;
 
 function makeDocs(mod) {
     console.log("making checkmight docs...");
-    // TODO: open output file
-    // TODO: read template file
 
     // get all types
     let {TypeCheck} = mod;
@@ -17,12 +19,9 @@ function makeDocs(mod) {
     let flatList = flattenTypes(typeList);
     console.log("flatList", inspect(flatList, {showHidden: false, depth: null}));
 
-    // TODO: iterate type examples
-    // TODO: iterate attributes / does examples
-
-    printTypeIndex(flatList.map((t) => t.typeName));
-    printAttrIndex(flatList);
-    printBody(flatList);
+    createIndex(`${OUTPUT_FOLDER}/index.md`, flatList);
+    createIndex("README.md", flatList);
+    createDocs(flatList);
 }
 
 function flattenTypes(typeList) {
@@ -30,12 +29,16 @@ function flattenTypes(typeList) {
     typeList.forEach((type) => {
         let typeObj = {
             typeName: type.name,
+            typeDoc: type.documentation,
             attrs: [],
             does: [],
             examples: [],
         };
 
-        // TODO: type desc
+        // TODO: type examples
+        type.examples.forEach((ex) => {
+            typeObj.examples.push(`${ex.runTest() ? "+" : "-"} ${ex.toString()}`);
+        });
 
         type.attrs.forEach((value, key) => {
             let attrObj = {
@@ -46,6 +49,10 @@ function flattenTypes(typeList) {
 
             // TODO: attr desc
             // TODO: attr examples
+            // type.attrs.examples.forEach((ex) => {
+            //     attrObj.examples.push(`${ex.runTest() ? "+" : "-"} ${ex.toString()}`);
+            //     typeObj.attrs.push(attrObj);
+            // });
             typeObj.attrs.push(attrObj);
         });
 
@@ -61,7 +68,6 @@ function flattenTypes(typeList) {
             typeObj.does.push(doesObj);
         });
 
-        // TODO: type examples
         typeObj.attrs = typeObj.attrs.sort(attrSort);
         typeObj.does = typeObj.does.sort(doesSort);
         flatList.push(typeObj);
@@ -71,43 +77,81 @@ function flattenTypes(typeList) {
     return flatList;
 }
 
-function printTypeIndex(idx) {
-    print("");
-    print("## Type Index");
-    idx.forEach((v) => {
-        print(`* [${v}](#${v}): is${v} / isNot${v} / isOpt${v}`);
-    });
-    print("");
+function createIndex(filename, idx) {
+    let fd = fs.openSync(filename, "w");
+    let template = getTemplateFile();
+    print(fd, template);
+    print(fd, "");
+    writeTypeIndex(fd, idx);
+    writeAttrIndex(fd, idx);
 }
 
-function printAttrIndex(idx) {
-    print("");
-    print("## Attribute Index");
-    idx.forEach((t) => {
-        print(`* [${t.typeName}](#${t.typeName})`);
-        t.attrs.forEach((a) => {
-            print(`    * [${a.attrName}](#${t.typeName}-${a.attrName})`);
-        });
-        t.does.forEach((d) => {
-            print(`    * [${d.descName}](#${t.typeName}-${d.descName})`);
-        });
-    });
-    print("");
+function getTemplateFile() {
+    return fs.readFileSync(TEMPLATE_FILE, "utf8");
 }
 
-function printBody(idx) {
+function writeTypeIndex(fd, idx) {
+    print(fd, "");
+    print(fd, "## Type Index");
+    idx.forEach((type) => {
+        print(fd, `* [${type.typeName}](${URL_ROOT}${type.typeName.toLowerCase()}.md): is${type.typeName} / isNot${type.typeName} / isOpt${type.typeName}`);
+    });
+    print(fd, "");
+}
+
+function writeAttrIndex(fd, idx) {
+    print(fd, "");
+    print(fd, "## Attribute Index");
     idx.forEach((t) => {
-        print("");
-        print(`## ${t.typeName}()`);
+        print(fd, `* [${t.typeName}](#user-content-${t.typeName.toLowerCase()})`);
+        t.attrs.forEach((a) => {
+            print(fd, `    * [has${a.attrName}](#user-content-is${t.typeName.toLowerCase()}has${a.attrName.toLowerCase()})`);
+        });
         t.does.forEach((d) => {
-            print("");
-            print(`### ${t}().${d}()`);
+            print(fd, `    * [does${d.doesName}](#user-content-is${t.typeName.toLowerCase()}does${d.doesName.toLowerCase()})`);
+        });
+    });
+    print(fd, "");
+}
+
+function createDocs(idx) {
+    idx.forEach((t) => {
+        let fd = fs.openSync(`${OUTPUT_FOLDER}/${t.typeName.toLowerCase()}.md`, "w");
+        print(fd, `## ${t.typeName}`);
+
+        // print type documentation
+        if (t.typeDoc) {
+            print(fd, `**Description:** ${t.typeDoc}`);
+        } else {
+            warn(`No documentation for type: ${t.typeName}`);
+        }
+
+        if (t.examples.length) {
+            print(fd, "");
+            print(fd, "**Examples:**");
+            print(fd, "``` diff");
+            print(fd, t.examples.join("\n"));
+            print(fd, "```");
+        } else {
+            warn(`No examples for type: ${t.typeName}`);
+        }
+
+        t.does.forEach((d) => {
+            print(fd, "");
+            print(fd, `### is${t.typeName}().does${d.doesName}()`);
         });
         t.attrs.forEach((a) => {
-            print("");
-            print(`### ${t}().${a}()`);
+            print(fd, "");
+            print(fd, `### is${t.typeName}().has${a.attrName}()`);
+            print(fd, "");
+            // if (t.attrs.examples.length) {
+            //     print("**Examples:**");
+            //     print(a.examples.join("\n"));
+            // } else {
+            //     warn(`No examples for attribute is${t.typeName}.has${a.attrName}`);
+            // }
         });
-        print("");
+        print(fd, "");
     });
     // TODO: print type name
     // TODO: print type description
@@ -161,4 +205,4 @@ const doesSort = objSort.bind(null, "doesName");
 //     return 0;
 // }
 
-plugin(makeDocs);
+require("..").plugin(makeDocs);
